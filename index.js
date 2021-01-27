e52e632d87d6608cfdf47afdee25021d84de1a3f
Zach Stearns <zach.stearns92@gmail.com>
2021-01-26 19:07:46 -0700

    Add GET footnotes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
+const { response } = require('express');
 const database = require('knex')(configuration);
 
 app.use(bodyParser.json());
@@ -28,3 +29,13 @@ app.get('/api/v1/papers', (request, response) => {
       response.status(500).json({ error });
     });
 });
+
+app.get('/api/v1/footnotes', (request, response) => {
+  database('footnotes').select()
+    .then((footnotes) => {
+      response.status(200).json(footnotes);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
+})
