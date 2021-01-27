0f6f306d202b3b0f4f4e17c515df4b0dd028bde5
Zach Stearns <zach.stearns92@gmail.com>
2021-01-26 19:24:23 -0700

    Add POST method for footnotes

--- index.js
+++ index.js
@@ -59,3 +59,23 @@ app.post('/api/v1/papers', (request, response) => {
       response.status(500).json({ error });
     });
 });
+
+app.post('/api/v1/footnotes', (request, response) => {
+  const footnote = request.body;
+
+  for (let requiredParameter of ['notes', 'paper_id']) {
+    if (!footnote[requiredParameter]) {
+      return response
+        .status(422)
+        .send({ error: `Expected format: { title: <String>, author: <String> }. You're missing a "${requiredParameter}" property.` });
+    }
+  }
+
+  database('footnotes').insert(footnote, 'id')
+    .then(footnote => {
+      response.status(201).json({ id: footnote[0] })
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
